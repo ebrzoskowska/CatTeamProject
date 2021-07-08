7723492a4fdf263c9f4ae3e7e21cbe0f0ec1d01f
Gaby Hall <gabyhall@Jonathan-Robertss-iMac.local>
2021-07-08 20:42:08 +0100

    gaby-cart v1

--- src/pages/AllProducts.js
+++ src/pages/AllProducts.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import classes from './AllProducts.module.css';
+// import CartPage from "./Cart";
 
 const AllProductsPage = () => {
 
@@ -12,7 +13,14 @@ const AllProductsPage = () => {
     const info = await response.json();
     setInfo(info)
 }   
-  
+ 
+  const [name, setName] = useState("");
+  const [origin, setOrigin] = useState("");
+  const [price, setPrice] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+
+  const [showCart, setShowCart] = useState(false);
+
 // useEffect trigger infoFetch to fetch images from Api after page onload
   useEffect(() => {
     infoFetch()
@@ -21,22 +29,56 @@ const AllProductsPage = () => {
 // map method looping throug array ofcat images
   const CatPics = () => {
     return (
+    <div>
+    <div>
+        <Panel /> 
+        {/* work out how to put to the side later */}
+      </div> 
     <div className={classes.cats}>  
         {info.map((item) => {
-            return (
+           return (
+                
                 <div className={classes.catPics}>
                     <img className={classes.allProductsImages} src={item.image.url} alt="cat"></img>
                     <p  className={classes.info}>name: {item.name}</p>
                     <p className={classes.info}>origin: {item.origin}</p>
                     <p className={classes.info}>£{item.image.width}</p>
-                    <button className={classes.btn} >Add kitty!</button>
+                    <button className={classes.btn} onClick={() => 
+                    {const name = item.name;  // I know this is messy - I put it here because when it was in its own function it wasn't accessing the values
+                      setName(name); 
+                      const origin = item.origin;
+                      setOrigin(origin); 
+                      const price = item.image.width;
+                      setPrice(price); 
+                      setShowCart(true);
+                    }}>Add kitty!</button>
                 </div>
             );
         })}
     </div> 
+    </div>
     );
   };   
-  
+
+
+const Panel = () => {
+
+const totalPrice = price + price;
+setTotalPrice(totalPrice)
+
+  return( 
+   <div className="cart" style={showCart ? {} : {display: 'none'}}>  
+   {/* style stops it appearing when empty using showCart useState */}
+    <h2>Your Cart</h2>
+    <p>{name}</p>
+    <p>{origin}</p>
+    <p>£{price}</p>
+    <h3>Total Price: {totalPrice}</h3>
+  </div>
+  )
+}
+
+
 // return images from data array    
 return (
   <div>          
@@ -44,5 +86,6 @@ return (
     </div>
   ); 
 };
-  
+
+
 export default AllProductsPage;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,22 @@
+
+import classes from './AllProducts.module.css';
+
+
+
 function CartPage() {
+    const {name} = require('./AllProducts.js')
+    console.log(name)
     return (
         <section>
-            <h1></h1>
+            <h1>Your cart</h1>
+            <div>
+            <p>Your kitties:</p>
+                    <p className={classes.info}>Name: {name}</p>
+                    <p className={classes.info}>Origin:  </p>
+                    <p className={classes.info}>Price: £</p> 
+                    {/* this doesn't work, tried it on AllProducts page as pop-up panel instead. If that works, maybe delete this page? */}
+            <h2>Total price:</h2>
+            </div>
         </section>
     )
 }
